7aa38a27aba777410766f70590d261128f08c2fb
arcolinuxz <arcolinuxinfo@gmail.com>
2022-03-10 12:50:43 +0100

    first commit

--- git-v2.sh
+++ git-v2.sh
@@ -39,7 +39,7 @@ git commit -m "$input"
 
 # Push the local files to github
 
-git push -u origin master
+git push -u origin main
 
 
 echo "################################################################"
